2122f3dd9d5e843a9b4d9574be8d3582aeeb3324
Archerymystery <84961834+Archerymystery@users.noreply.github.com>
2026-06-19 23:14:30 +0200

    feat: create Maze struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use colored::*;
+#[derive(Debug, Clone)]
+enum MazeSquare {
+    Entrance,
+    Exit,
+    Road,
+    Wall,
+    Trap,
+    Treasure,
+}
+struct Maze {
+    width: usize,
+    height: usize,
+    data: Vec<MazeSquare>,
+}
+fn generate_maze(width: usize, height: usize) -> Maze {
+    let mut data: Vec<MazeSquare> = vec![MazeSquare::Wall; width * height];
+
+    Maze {
+        width,
+        height,
+        data,
+    }
+}
+fn print_maze(maze: Maze) {
+    for row in 0..maze.height {
+        for col in 0..maze.width {
+            let index = row * maze.width + col;
+            match maze.data[index] {
+                MazeSquare::Entrance => print!("{}", "██".green()),
+                MazeSquare::Exit => print!("{}", "██".blue()),
+                MazeSquare::Road => print!("  "),
+                MazeSquare::Wall => print!("██"),
+                MazeSquare::Trap => print!("{}", "██".red()),
+                MazeSquare::Treasure => print!("{}", "██".yellow()),
+            }
+        }
+        println!();
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let maze = generate_maze(10, 10);
+    print_maze(maze);
 }
