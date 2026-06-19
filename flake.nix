4cc31eb02626744f293726ac06e3ec90e7ecd52a
Archerymystery <84961834+Archerymystery@users.noreply.github.com>
2026-06-19 22:45:08 +0200

    chore: initialize Rust project using cargo init

---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  description = "Nix flake for rust";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ rust-overlay.overlays.default ];
+      };
+      rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+        extensions = [
+          "rust-src"
+          "rustfmt"
+          "rust-analyzer"
+        ];
+      };
+      rustPlatform = pkgs.makeRustPlatform {
+        cargo = rustToolchain;
+        rustc = rustToolchain;
+      };
+      orderbookPkg = rustPlatform.buildRustPackage {
+        pname = "maze";
+        version = "0.1.0";
+
+        src = ./.;
+
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+        };
+
+        nativeBuildInputs = with pkgs; [ pkg-config ];
+      };
+    in
+    {
+      packages.${system} = {
+        default = orderbookPkg;
+        Orderbook = orderbookPkg;
+      };
+      apps.${system}.default = {
+        type = "app";
+        program = "${orderbookPkg}/bin/Orderbook";
+      };
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          rustToolchain
+          pkg-config
+        ];
+
+        RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+      };
+    };
+}
